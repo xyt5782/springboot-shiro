99ede8d148f422fdca365d4d2954789ecc100011
xyt5782 <37906070+xyt5782@users.noreply.github.com>
2018-08-15 21:26:12 +0800

    Update Newtest.java

--- src/main/java/com/yt/logging/me/Newtest.java
+++ src/main/java/com/yt/logging/me/Newtest.java
@@ -7,5 +7,7 @@ package com.yt.logging.me;
 public class Newtest {
     public static void main(String[] args) {
         System.out.println("hhh");
+        System.out.println("hhh");
+        
     }
 }
